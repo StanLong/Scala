ce93446331afef8bc2b2472bf414ee27819e057b
StanLong <youlinbhz@sina.cn>
2021-03-16 20:52:55 +0800

    流程控制作业

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -3,17 +3,28 @@ package com.stanlong.scala
 import scala.io.StdIn
 
 /**
- * 从控制台接收信息
+ * 流程控制
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        println("请输入姓名: ")
-        val name = StdIn.readLine()
-        print("请输入年龄: ")
-        val age = StdIn.readInt()
-        print("请输入薪资: ")
-        val salary = StdIn.readDouble()
-        printf("姓名%s, 年龄%d, 薪资%f", name, age, salary)
-
+        print("请输入参数a的值")
+        val a = StdIn.readInt()
+        print("请输入参数b的值")
+        val b = StdIn.readInt()
+        print("请输入参数c的值")
+        val c = StdIn.readInt()
+        val num = math.pow(b,2) - 4*a*c
+        var x = 0.0
+        var y = 0.0
+        if(num>0){
+            x = (-b + math.sqrt(num))/2*a
+            y = (-b - math.sqrt(num))/2*a
+            print(s"实根x: ${x}, 实根y: ${y}")
+        }else if((math.pow(b,2) - 4*a*c)==0){
+            x = -b/2*a
+            print(s"实根x: ${x}")
+        }else{
+            print("该方程无实根")
+        }
     }
 }
