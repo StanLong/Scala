d9cc1fb62bb79fa2b7bd01d31408a4aaf1baa0fe
StanLong <youlinbhz@sina.cn>
2021-03-20 23:57:49 +0800

    函数式编程

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,22 +1,20 @@
 package com.stanlong.scala
 
-import scala.util.control.Breaks.{break, breakable}
-
 /**
- * 循环中断
- * breakable 是一个高阶函数：可以接收函数作为参数的函数
+ * 可变形参
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        var n = 10
-        breakable{
-            while (n < 20){
-                n = n+1
-                if(n==18){
-                    break()
-                }
-                println(n)
-            }
+        var result = sum(10, 20, 30, 40)
+        println(result)
+
+    }
+    // args:Int* 可变形参，可变形参得是最后一个参数
+    def sum(num1:Int, args:Int*):Int ={
+        var sum = num1;
+        for(item <- args){
+            sum = sum + item
         }
+        sum
     }
 }
