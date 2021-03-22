a7f17de25728abc749f7b1b278d114d2e889d93c
StanLong <youlinbhz@sina.cn>
2021-03-22 23:22:26 +0800

    惰性函数

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,16 +1,17 @@
 package com.stanlong.scala
 
 /**
- * 可变形参
+ * 惰性函数
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        var result = sum(10, 20, 30, 40)
-        println(result)
-
+        lazy val result = sum(10,20) // 这段使用了lazy， 函数中的println("测试懒加载")并不会执行
+        println("----------------")
+        println("result = " + result) // 只有在函数调用的时候， 函数中的println("测试懒加载")才会执行
     }
     // args:Int* 可变形参，可变形参得是最后一个参数
     def sum(num1:Int, args:Int*):Int ={
+        println("测试懒加载")
         var sum = num1;
         for(item <- args){
             sum = sum + item
