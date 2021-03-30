ba06e468216991b318698bab5512e564a6a36559
StanLong <youlinbhz@sina.cn>
2021-03-30 23:38:21 +0800

    异常

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,21 +1,16 @@
 package com.stanlong.scala
 
+import scala.io.StdIn
+
 /**
- * 惰性函数
+ * 异常注解
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        lazy val result = sum(10,20) // 这段使用了lazy， 函数中的println("测试懒加载")并不会执行
-        println("----------------")
-        println("result = " + result) // 只有在函数调用的时候， 函数中的println("测试懒加载")才会执行
-    }
-    // args:Int* 可变形参，可变形参得是最后一个参数
-    def sum(num1:Int, args:Int*):Int ={
-        println("测试懒加载")
-        var sum = num1;
-        for(item <- args){
-            sum = sum + item
+        print("请输入金字塔的层级:")
+        var num = StdIn.readInt()
+        for(i <- 1 to num){
+            println(" " * (num-i) + "*" * ((2 * i)-1))
         }
-        sum
     }
 }
