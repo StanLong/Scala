f43493e4e78b11017a011ab3577c860a838ae095
StanLong <youlinbhz@sina.cn>
2021-04-12 22:19:40 +0800

    面向对象

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,42 +1,30 @@
 package com.stanlong.scala
 
+import scala.beans.BeanProperty
+
 /**
  * 计算字符串中所有字母的Unicode代码的乘积
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        val person = new Person("Stanlong", 20)
-        println(person)
+        var car = new Car
+        car.name = "宝马"
+        println(car.name)
 
-        val person2 = new Person("南海财神") // 调用辅助构造器
-        println(person2)
-
-        person.showInfo()
-        person2.showInfo()
+        car.setName("奔驰")
+        println(car.getName())
     }
 }
 
-class Person() {
-    var name: String = _
-    var age: Int = _
-    def this(name : String) {
-        //辅助构造器无论是直接或间接，最终都一定要调用主构造器，执行主构造器的逻辑
-        //而且需要放在辅助构造器的第一行[这点和java一样，java中一个构造器要调用同类的其它构造器，也需要放在第一行]
-        this()  //直接调用主构造器
-        this.name = name
-    }
-    def this(name : String, age : Int) {
-        this() //直接调用主构造器
-        this.name = name
-        this.age = age
-    }
-    def this(age : Int) {
-        this("匿名") //简介调用主构造器,因为 def this(name : String) 中调用了主构造器!
-        this.age = age
-    }
-    def showInfo(): Unit = {
-        println("person信息如下:")
-        println("name=" + this.name)
-        println("age=" + this.age)
+class Person {
+    var age: Short = 90
+    var name: String
+    = _
+    def this (n: String, a: Int) {
+        this ()
+        this.name = n
+        this.age = a
     }
 }
+
+var p: Person = new Person ("小倩", 20)
