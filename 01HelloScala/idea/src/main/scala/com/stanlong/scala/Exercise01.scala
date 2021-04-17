1878e4c0c031c39835a859a2d54ff56cae16a071
StanLong <youlinbhz@sina.cn>
2021-04-17 14:39:13 +0800

    面向对象

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,30 +1,50 @@
 package com.stanlong.scala
 
-import scala.beans.BeanProperty
-
 /**
- * 计算字符串中所有字母的Unicode代码的乘积
+ * 抽象类
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        var car = new Car
-        car.name = "宝马"
-        println(car.name)
+        val bank = new Bank("StanLong", 100.00, "123456")
+        //bank.query("StanLong", "123456")
+        //bank.withdraw("StanLong", "123456", 10.00)
+        bank.save("StanLong", "123456", 10.00)
 
-        car.setName("奔驰")
-        println(car.getName())
     }
 }
 
-class Person {
-    var age: Short = 90
-    var name: String
-    = _
-    def this (n: String, a: Int) {
-        this ()
-        this.name = n
-        this.age = a
+/**
+ * 属性：账号，余额，密码
+ * 方法：查询，存款，取款
+ */
+class Bank(inAccount:String, inBalance:Double, inPwd:String){
+    val accountNo = inAccount
+    val balance = inBalance
+    var pwd = inPwd
+
+    // 查询
+    def query(accountNo:String, pwd:String):Unit={
+        if(!this.accountNo.equals(accountNo) && !this.pwd.equals(pwd)){
+            println("用户名或密码错误！")
+            return
+        }
+        printf("账号为 %s 当前余额是%.2f\n", this.accountNo, this.balance)
+    }
+
+    // 取钱
+    def withdraw(accountNo:String, pwd:String, balance:Double): Unit ={
+        if(balance > this.balance){
+            println("余额补足")
+        }else{
+            printf("%s 取出金额 %.2f， 剩余金额%.2f", this.accountNo, this.balance, (this.balance-balance))
+        }
+    }
+
+    // 存钱
+    def save(accountNo:String, pwd:String, balance:Double): Unit ={
+        printf("%s 取出金额 %.2f， 剩余金额%.2f", this.accountNo, this.balance, (this.balance+balance))
     }
 }
 
-var p: Person = new Person ("小倩", 20)
+
+
