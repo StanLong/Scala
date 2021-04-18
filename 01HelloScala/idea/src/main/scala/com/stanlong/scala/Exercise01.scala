bb8371377da3634686d05ad815a148633c434d33
StanLong <youlinbhz@sina.cn>
2021-04-18 21:02:04 +0800

    面向对象

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,48 +1,31 @@
 package com.stanlong.scala
 
 /**
- * 抽象类
+ * Time 类，判断事件
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        val bank = new Bank("StanLong", 100.00, "123456")
-        //bank.query("StanLong", "123456")
-        //bank.withdraw("StanLong", "123456", 10.00)
-        bank.save("StanLong", "123456", 10.00)
+        val cur = new Time(10, 20)
+        val other = new Time(10, 20)
+        println(other.before(cur))
 
     }
 }
 
 /**
- * 属性：账号，余额，密码
- * 方法：查询，存款，取款
+ * Time 类，判断事件
  */
-class Bank(inAccount:String, inBalance:Double, inPwd:String){
-    val accountNo = inAccount
-    val balance = inBalance
-    var pwd = inPwd
-
-    // 查询
-    def query(accountNo:String, pwd:String):Unit={
-        if(!this.accountNo.equals(accountNo) && !this.pwd.equals(pwd)){
-            println("用户名或密码错误！")
-            return
-        }
-        printf("账号为 %s 当前余额是%.2f\n", this.accountNo, this.balance)
-    }
-
-    // 取钱
-    def withdraw(accountNo:String, pwd:String, balance:Double): Unit ={
-        if(balance > this.balance){
-            println("余额补足")
+class Time(hrs:Int, min:Int){
+    val hours = hrs
+    val minutes = min
+    def before(other:Time):Boolean={
+        if(hours<other.hours){
+            true
+        }else if(hours > other.hours){
+            false
         }else{
-            printf("%s 取出金额 %.2f， 剩余金额%.2f", this.accountNo, this.balance, (this.balance-balance))
+            if(minutes<other.minutes) true else false
         }
-    }
-
-    // 存钱
-    def save(accountNo:String, pwd:String, balance:Double): Unit ={
-        printf("%s 取出金额 %.2f， 剩余金额%.2f", this.accountNo, this.balance, (this.balance+balance))
     }
 }
 
