2813c1f94c45bad1b1a3f804f37437b0cf902967
StanLong <youlinbhz@sina.cn>
2021-05-06 23:01:37 +0800

    封装

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,33 +1,32 @@
 package com.stanlong.scala
 
 /**
- * Time 类，判断事件
+ * 继承
  */
-object Exercise01 {
+object Exercise01{ // 继承App后，就可以直接在这个类中执行代码，不需要再写main入口
     def main(args: Array[String]): Unit = {
-        val cur = new Time(10, 20)
-        val other = new Time(10, 20)
-        println(other.before(cur))
-
-    }
-}
-
-/**
- * Time 类，判断事件
- */
-class Time(hrs:Int, min:Int){
-    val hours = hrs
-    val minutes = min
-    def before(other:Time):Boolean={
-        if(hours<other.hours){
-            true
-        }else if(hours > other.hours){
-            false
-        }else{
-            if(minutes<other.minutes) true else false
-        }
+        val student = new Student
+        student.name = "StanLong"
+        student.age = 27
+        student.studying()
+        student.showInfo()
     }
 }
 
 
+class Person(){
+    var name : String = _
+    var age : Int = _
 
+    def showInfo(): Unit = {
+        println("学生信息如下")
+        println("名字: " + this.name)
+        println("年龄: " + this.age)
+    }
+}
+
+class Student extends Person{
+    def studying(): Unit = {
+        println(this.name + " 学习scala...")
+    }
+}
