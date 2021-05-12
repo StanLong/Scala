a37eccf9d107dd4ba518517922e4f4cc6e80dd77
StanLong <youlinbhz@sina.cn>
2021-05-12 20:46:09 +0800

    面向对象

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,31 +1,28 @@
 package com.stanlong.scala
 
 /**
- * 属性重写
+ * 只有主构造器可以调用父类的构造器。辅助构造器不能直接调用父类的构造器
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        val person = new Person
-        val student = new Student
-        println(person.name)
-        println(student.name)
-
-        val person2 = new Person
-        val student2 = new Person
-        println(person2.name)
-        println(student2.name)
-
-        val person3 = new Student
-        val student3 = new Student
-        println(person3.name)
-        println(student3.name)
+        val stu = new Student("StanLong")
+        stu.printName()
     }
 }
 
-class Person{
-    val name:String = "StanLong"
+class Person( pname : String )  {
+    var name : String = pname
+    def printName() {
+        println("Person printName() " + name)
+    }
 }
 
-class Student extends Person{
-    override val name:String = "南海财神"
+class Student(studentname : String ) extends  Person(studentname)  {
+
+    var sno : Int = 20
+
+    override def printName() {
+        println("Student printName() " + name)
+        super.printName()
+    }
 }
