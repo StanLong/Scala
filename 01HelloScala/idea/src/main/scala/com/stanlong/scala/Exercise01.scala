b3d44e2c7c528aa3635ec4a1a92d20be171ebf5a
StanLong <youlinbhz@sina.cn>
2021-05-16 09:42:46 +0800

    内部类

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,36 +1,39 @@
 package com.stanlong.scala
 
 /**
- * 扩展类的特质
+ * 嵌套类-》类型投影
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
+        val outer1 : ScalaOuterClass = new ScalaOuterClass();
+        val outer2 : ScalaOuterClass = new ScalaOuterClass();
+        // Scala创建内部类的方式和Java不一样，将new关键字放置在前，使用  对象.内部类  的方式创建
+        val inner1 = new outer1.ScalaInnerClass()
+        val inner2 = new outer2.ScalaInnerClass()    //创建静态内部类对象
+
+        //说明下面调用test 的 正确和错误的原因：
+        //1.Java中的内部类从属于外部类,因此在java中 inner.test(inner2) 就可以，因为是按类型来匹配的。
+        //2 Scala中内部类从属于外部类的对象，所以外部类的对象不一样，创建出来的内部类也不一样，无法互换使用
+        //3. 比如你使用ideal 看一下在inner1.test()的形参上，
+        // 它提示的类型是 outer1.ScalaOuterClass, 而不是ScalaOuterClass
+
+        inner1.test(inner1) // ok
+        // inner1.test(inner2) // 错误
     }
 }
 
-//Logger就是自身类型特质
-trait Logger{
-    // 明确告诉编译器，我就是Exception,如果没有这句话，下面的getMessage不能调用
-    this: Exception =>
-    def log(): Unit ={
-        // 既然我就是Exception, 那么就可以调用其中的方法
-        println(getMessage)
+class ScalaOuterClass {
+    myOuter =>  class ScalaInnerClass {
+        //成员内部类
+        def test( ic : ScalaInnerClass ) : Unit = {
+            System.out.println(ic);
+        }
     }
 }
 
-//下面会报错 llegal inheritance, self-type Console does not conform to Exception
-//分析原因
-//1. Logger 已经是Exception了
-//2. 但是这里Console 并没有说明是Excepton的子类，因此出现了
-// self-type Console does not conform to Exception，即自身类型的约束。
-//class Console extends  Logger { //错误×
-//
-//}
-
-// 如果我们需要混入Logger这种自身类型的特质，需要让该类也继承Excetion
-// 这样Console 才能混入Logger,保证都是Excetpion类型
-
-// 此处必须继承Exception类，否则无法混入logger特质
-class Console extends Exception with Logger {
-
+object ScalaOuterClass {
+    //伴生对象
+    class ScalaStaticInnerClass {
+        //静态内部类
+    }
 }
