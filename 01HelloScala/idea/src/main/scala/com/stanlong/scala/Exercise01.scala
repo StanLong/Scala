2c97012a1f07813671893d1fe4b4cc7cffa3d466
StanLong <youlinbhz@sina.cn>
2021-05-22 14:48:28 +0800

    隐式转换

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,42 +1,26 @@
 package com.stanlong.scala
 
 /**
- * 嵌套类-》类型投影
- * 类型投影主要用在有外部类和内部类，并且外部类有方法是需要传入内部类作参数的时候用的
- * 而这个传入的参数要求除了可以是自己这个对象的内部类，也可以是其他实例对象的内部类，只要都是实例化这个类就行了。
- * 格式是 Outter#Inner
+ * 隐式类
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        val outer1 : ScalaOuterClass = new ScalaOuterClass();
-        val outer2 : ScalaOuterClass = new ScalaOuterClass();
-        // Scala创建内部类的方式和Java不一样，将new关键字放置在前，使用  对象.内部类  的方式创建
-        val inner1 = new outer1.ScalaInnerClass()
-        val inner2 = new outer2.ScalaInnerClass()    //创建静态内部类对象
+        // DB 会对应生成隐式类，当在该隐式类的作用域范围创建MySql 类的实例时，该隐式类就会生效
+        // 隐式类必须被定义在“类”或“伴生对象”或“包对象”里，即隐式类不能是 顶级的
+        implicit class DB(val m: MySql){ // 其所带的构造参数有且只能有一个
+            def addSuffix():String={
+                m + "scala"
+            }
+        }
 
-        /**
-         * Scala中的内部类 必须依赖于外部类的实例 而外部类的实例各不相同 所以被之为这种对于外部类的依赖为"路径依赖"
-         * 所以不同的路劲代表不同的类型, 即这里inner1和inner2并不是同一个类型
-         */
-
-        inner1.test(inner1) // ok
-        // inner1.test(inner2) // 错误
-
-        inner1.test2(inner1) // ok
-        inner1.test2(inner2) // ok
+        val mySql = new MySql
+        mySql.sayOK()
+        mySql.addSuffix()
     }
 }
 
-class ScalaOuterClass {
-    myOuter =>  class ScalaInnerClass {
-        //成员内部类
-        def test( ic : ScalaInnerClass ) : Unit = {
-            System.out.println(ic);
-        }
-
-        //类型投影
-        def test2(ScalaInnerClass: ScalaOuterClass#ScalaInnerClass) : Unit = {
-            System.out.println(ScalaInnerClass);
-        }
+class MySql{
+    def sayOK(): Unit ={
+        println("Ok~")
     }
 }
