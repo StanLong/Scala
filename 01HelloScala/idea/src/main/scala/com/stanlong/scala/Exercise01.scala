17bcc90653fdcec82b681bfaa75290fe2ca72603
StanLong <youlinbhz@sina.cn>
2021-05-25 23:05:02 +0800

    集合

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,16 +1,12 @@
 package com.stanlong.scala
 
 /**
- * List
+ * Queue
+ * 队列是一个有序列表，在底层可以用数组或是链表来实现
+ * 其输入和输出要遵循先入先出的原
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        // 在scala中List默认为不可变的集合
-        // 如果需要使用可变List， 则使用ListBuffer
-        val list01 = List(1, 2, 3) //创建时，直接分配元素 List 中可以放任何数据类型
-        println(list01)
-        val list02 = Nil  //空集合
-        println(list02)
 
     }
 }
