0b1258ba50fe246069db77a3e10711eeb9d713f9
StanLong <youlinbhz@sina.cn>
2021-05-30 16:07:49 +0800

    高阶函数

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,19 +1,25 @@
 package com.stanlong.scala
 
 /**
- * 高阶函数
+ * 扫描
+ * 扫描，即对某个集合的所有元素做fold操作，但是会把产生的所有中间结果放置于一个集合中保存
  */
 object Exercise01 {
     def main(args: Array[String]): Unit = {
-        val list = List(3,5,7)
-        // list.map 的执行过程
-        // 将list集合中的元素依次遍历
-        // 将遍历出的元素传递给multiple函数， 并将得到的返回值放入一个新的集合
-        val list2 = list.map(multiple)
-        println(list2)
+
+        //5 (1,2,3,4,5) =>(5,4,2,-1,-5,-10)
+        val i8 = (1 to 5).scanLeft(5)(minus) //IndexedSeq[Int]
+        println(i8)
+
+        //5 (1,2,3,4,5) =>(5,6,8, 11,15,20)
+        val i9 = (1 to 5).scanLeft(5)(add) //IndexedSeq[Int]
+        println(i9)
+    }
+    def minus( num1 : Int, num2 : Int ) : Int = {
+        num1 - num2
     }
 
-    def multiple(n:Int):Int={
-        2 * n
+    def add( num1 : Int, num2 : Int ) : Int = {
+        num1 + num2
     }
 }
