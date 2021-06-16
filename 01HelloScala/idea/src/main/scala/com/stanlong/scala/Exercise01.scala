cf9bffac53f56b7a8ff920fbb536e20ba336712e
StanLong <youlinbhz@sina.cn>
2021-06-16 22:06:53 +0800

    模式匹配

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,54 +1,23 @@
 package com.stanlong.scala
 
 /**
- * 匹配对象
- *
+ * for表达式中的模式
  */
-object Exercise01 {
+object Exercise01{
     def main(args: Array[String]): Unit = {
-        /*
-        * 构建对象时apply会被调用 ，比如 val n1 = Square(5)
-        * 当将 Square(n) 写在 case 后时[case Square(n) => xxx]，会默认调用unapply 方法(对象提取器)
-        * number 会被 传递给def unapply(z: Double) 的 z 形参
-        * 如果返回的是Some集合，则unapply提取器返回的结果会返回给 n 这个形参
-        * case中对象的unapply方法(提取器)返回some集合则为匹配成功
-        * 返回none集合则为匹配失败
-        */
-
-        val number: Double = 36.0
-        number match {
-            case Square(n) => println(n)
-            case _ => println("nothing matched")
+        val map = Map("A"->1, "B"->0, "C"->3)
+        for ( (k, v) <- map ) {
+            println(k + " -> " + v)
         }
-/////////////////////////////////////////////////////////////////////
-        /*
-         * 当case 后面的对象提取器方法的参数为多个，则会默认调用def unapplySeq() 方法
-         * 如果unapplySeq返回是Some，获取其中的值,判断得到的sequence中的元素的个数是否是三个,如果是三个，则把三个元素分别取出，赋值给first，second和third
-         * 其它的规则不变
-         */
-        val namesString = "Alice,Bob,Thomas"
-        //说明
-        namesString match {
-            case Names(first, second, third) => {
-                println("the string contains three people's names")
-                // 打印字符串
-                println(s"$first $second $third")
-            }
-            case _ => println("nothing matched")
+        for ((k, 0) <- map) {
+            // for中匹配会自动忽略失败的匹配，这里结果是 "B"->0，  "A"->1 和 "C"->3 会被忽略
+            println(k + " --> " + 0)
         }
 
-    }
-}
-
-object Square {
-    def unapply(z: Double): Option[Double] = Some(math.sqrt(z))
-    def apply(z: Double): Double = z * z
-}
+        for ((k, v) <- map if v == 0) {
+            println(k + " ---> " + v)
+        }
 
 
-object Names {
-    def unapplySeq(str: String): Option[Seq[String]] = {
-        if (str.contains(",")) Some(str.split(","))
-        else None
     }
 }
