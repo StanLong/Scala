0e7b8e5b781892804f80592aa887941c8992e4d1
StanLong <youlinbhz@sina.cn>
2021-06-19 12:39:20 +0800

    高阶函数

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,41 +1,83 @@
 package com.stanlong.scala
 
 /**
- * 商品捆绑打折出售
- *
- * 现在有一些商品，请使用Scala设计相关的样例类，完成商品捆绑打折出售。要求
- * 商品捆绑可以是单个商品，也可以是多个商品。
- * 打折时按照折扣x元进行设计.
- * 能够统计出所有捆绑商品打折后的最终价格
+ * 高阶函数
+ * 能够接受函数作为参数的函数，叫做高阶函数
  */
-object Exercise01{
+object Exercise01 {
     def main(args: Array[String]): Unit = {
+        // 案例一
+        val result = test(sum2, 3.5)
+        println(result)
 
-        val sale = Bundle("书籍", 10, Article("漫画", 40),
-            Bundle("文学作品", 20, Article("《阳关》", 80), Article("《围城》", 30)))
+        // 案例二
+        // 在scala中，可以把一个函数直接赋值给一个变量，但是不执行函数，使用下面这种写法
+        val f1 = myPrint _
+        f1() // 执行
 
-        def price(it: Item): Double = {
-            it match {
-                //说明
-                //1.p 就是 提取的该Aritlcle的价格
-                case Article(_, p) => p
-                //说明
-                //1.disc 是提取的Bundle 的折扣
-                //2.its@_* 是提取Bundle 所有的 Item* ,是一个集合
-                //3. its.map(price _).sum - disc 表示递归的对its进行map操作
-                //3.1 将从its 递归遍历取出的Aritcle对象传递给price 函数取出price 并累计求和 ,并减去折扣
-                //3.2 对每一个Bundle(因为可能多个) 递归的如上操作，最终得到最后的值
-                case Bundle(_, disc, its@_*) => its.map(price _).sum - disc
-            }
-        }
-        val amountMoney = price(sale)
-        println("amountMoney=" + amountMoney)
+        // 案例三
+        test2(sayOK)
+
+        // 案例四
+        val res = test4(sum, mod, 5.0)
+        println(res)
+
+        // 案例五
+        // 高阶函数可以返回函数类型
+        // 1. 分步执行
+        val f2 = minusxy(3) // 匿名函数f2 = 3-y
+        println(f2(1))  // 3-1
+        println(f2(9))  // 3-9
+
+        // 2. 一步到位执行
+        val result3 = minusxy(3)(5) // 返回的匿名函数可以用变量接受
+        println(result3)
+
     }
+
+    // 案例一
+    // test是一个高阶函数
+    // f:Double=>Double 表示一个函数，该函数可以接受一个Double， 返回double
+    // n1:Double 普通参数
+    // f(n1) 执行传入的函数
+    def test(f:Double=>Double, n1:Double)={
+        f(n1)
+    }
+
+    def sum2(d:Double):Double={
+        d + d
+    }
+
+    // 案例二
+    def myPrint(): Unit ={
+        println("Hello World")
+    }
+
+    // 案例三
+    // 说明test2是一个高阶函数，可以接受一个没有输入，返回哦Unit的函数
+    def test2(f: () => Unit) = {
+        f()
+    }
+    def sayOK() = {
+        println("sayOKKK...")
+    }
+
+    def test4(f:Double=>Double, f2:Double=>Int, n1:Double) ={
+        f(f2(n1))
+    }
+
+    def sum(d:Double):Double ={
+        d + d
+    }
+    def mod(d:Double):Int={
+        d.toInt % 2
+    }
+
+    // minusxy(3)执行minusxy(x: Int)得到 (y: Int) => 3 - y 这个匿名函
+    //minusxy(3)(5)执行 (y: Int) => x - y 这个匿名函数
+    //也可以分步执行: val f1 = minusxy(3);   val res = f1(9)
+    def minusxy(x: Int) = {
+        (y: Int) => x - y // 该函数返回一个匿名函数
+    }
+
 }
-
-abstract class Item
-
-case class Article(description: String, price: Double) extends Item
-
-case class Bundle(description: String, discount: Double, item: Item*) extends Item
-
