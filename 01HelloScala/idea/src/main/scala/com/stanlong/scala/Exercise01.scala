c303abda6d42ec2eca7f0c99bab676e32d0b75ec
StanLong <youlinbhz@sina.cn>
2021-06-20 15:58:52 +0800

    柯里化

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,32 +1,26 @@
 package com.stanlong.scala
 
 /**
- * 闭包
- * 请编写一个程序，具体要求如下
- * 编写一个函数 makeSuffix(suffix: String)  可以接收一个文件后缀名(比如.jpg)，并返回一个闭包
- * 调用闭包，可以传入一个文件名，如果该文件名没有指定的后缀(比如.jpg) ,则返回 文件名.jpg , 如果已经有.jpg后缀，则返回原文件名。
- * 要求使用闭包的方式完成
- * String.endsWith(xx)
+ * 抽象控制
+ *
+ * 进阶用法：实现类似while的until函数
  */
 object Exercise01 {
-    def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit ={
+        //说明
+        //1. 只要condition 为真，就不在执行 block代码
+        def until(condition: => Boolean)(block: => Unit) {//类似while循环，递归
+            if (!condition) {
+                block // block就是【x -= 1 println(x)】，会导致x的减小
+                //这里传入的始终是 () => {x == 0} 的匿名函数，但是x是变化,因此总会x==0成立的
+                until(condition)(block)
+            }
+        }
 
-        val f = makeSuffix(".jpg")
-        println(f("dog"))
-        println(f("cat"))
-        println(f("monster.jpg"))
-
-        // 返回的函数和 makeSuffix (suffix: String) 的 suffix 变量 和返回的函数组合成一个闭包,
-        // 因为 返回的函数引用到suffix这个变量
-        //我们体会一下这个闭包的好处，如果使用传统的方法，也可以轻松实现这个功能，但是传统方法需要每次都传入 后缀名，比如 .jpg ,
-        // 而闭包因为可以保留上次引用的某个值，所以我们传入一次就可以反复使用。大家可以仔细的体会一把！
-    }
-
-    def makeSuffix(suffix: String) = (name: String) => {
-        if (name.endsWith(suffix) == false) {
-            name + suffix
-        }else {
-            name
+        var x = 10
+        until(x == 0) {
+            x -= 1
+            println(x)
         }
     }
 }
