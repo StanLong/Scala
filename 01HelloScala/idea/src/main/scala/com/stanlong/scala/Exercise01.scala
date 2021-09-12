f4444ac3c84a85fa49f1911ea4d8c5768ae63aba
StanLong <youlinbhz@sina.cn>
2021-09-12 21:00:03 +0800

    scala复习

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,50 +1,40 @@
 package com.stanlong.scala
+import scala.util.control.Breaks.{break, breakable}
 
 /**
- * 空类型
- * Unit
- * null
- * Nothing
+ *  while循环
+ *  while
+ *  do..while
+ *  breakable 循环中断
  */
 object Exercise01 {
     def main(args: Array[String]): Unit ={
-        // 1. Unit
-        def m1():Unit={
-            println("m1被调用执行")
-        }
-        val a = m1()
-        println("a: " + a) // 打印结果 a: ()
-
-        // 2. null
-        // val b:Int = null // 报错： an expression of type Null is ineligible for implicit conversion
-        var student = new Student("张三", 20)
-        student = null
-        println(student) // 打印结果 null
-
-        // 3. Nothing
-        def m2(n:Int) :Nothing = {
-            throw new NullPointerException
+        // while
+        var a = 10
+        while( a>= 1){
+            println("this is a while loop: " + a)
+            a = a-1
         }
 
-        val b:Int = m2(2)
-        println("b: " + b) // 打印结果： Exception in thread "main" java.lang.NullPointerException
+        // do..while
+        var b = 10
+        do{
+            println("this is b while loop: " + b)
+            b = b-1
+        }while(b>0)
 
-        // 说明： Nothing是任何类型的子类型，如果有逻辑判断，函数方法可以做如下修改
-        def m3(n:Int):Int={
-            if(n == 0){
-                throw new NullPointerException
-            }else{
-                n
+        // breakable 循环中断
+        var n = 10
+        breakable{
+            while (n < 20){
+                n = n+1
+                if(n==18){
+                    break()
+                }
+                println(n)
             }
         }
 
-        val c:Int = m3(2)
-        println("c: " + c) // 打印结果：2
-
-
     }
 
-    class Student(name:String, age:Int){
-
-    }
 }
