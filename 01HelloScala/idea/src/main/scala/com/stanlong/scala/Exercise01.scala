1e8cde5f3f01ef787796a0ef5d10546421d6cc96
StanLong <youlinbhz@sina.cn>
2021-09-14 21:06:09 +0800

    scala复习-函数

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/Exercise01.scala
@@ -1,24 +1,12 @@
 package com.stanlong.scala
 
-/**
- * 匿名函数案例
- */
 object Exercise01 {
     def main(args: Array[String]): Unit ={
-        // 定义一个二元运算函数，只操作1和2两个数，但是具体运算通过参数传入
-        def dealFunOneAndTwo(fun:(Int, Int) => Int):Int={
-            fun(1,2)
-        }
-
-        val add = (a:Int, b:Int) => a + b
-        val minus = (a:Int, b:Int) => a-b
-
-        println(dealFunOneAndTwo(add))
-        println(dealFunOneAndTwo(minus))
-
-        // 匿名函数简化
-        println(dealFunOneAndTwo(_+_))
-        println(dealFunOneAndTwo(_-_))
+        val fun = (i:Int, s:String, c:Char) => {if(i==0 && s == "" && c=='0') false else true}
+        println(fun(0, "", '0'))
+        println(fun(1, "", '0'))
+        println(fun(0, "abc", '0'))
+        println(fun(0, "", '1'))
     }
 
 }
