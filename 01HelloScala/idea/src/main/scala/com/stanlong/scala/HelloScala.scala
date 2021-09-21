29ff51311c3e2c8ce47e604cb676573327ba21ff
StanLong <youlinbhz@sina.cn>
2021-09-21 20:37:49 +0800

    多态

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/HelloScala.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/HelloScala.scala
@@ -1,30 +1,38 @@
 package com.stanlong.scala
 
 /**
- * 继承
+ * 多态
+ * Java 中属性是静态绑定的，方法是动态绑定的
+ * Scala 中属性和方法都是动态绑定的，需要加 override 关键字
  */
 object HelloScala {
     def main(args: Array[String]): Unit = {
-        val student1 = new Student("zhangsan", 18)
-        val student2 = new Student("lisi", 19, "1142061140")
-        student1.showInfo()
-        student2.showInfo()
+        val student = new Student("lisi", 19, "1142061140")
+        student.showInfo()
+
+        val teacher = new Teacher
+        teacher.showInfo()
+
+        // 动态绑定方法
+        def personInfo(person:Person): Unit ={
+            person.showInfo()
+        }
+        personInfo(student)
+        personInfo(teacher)
+
+        // 动态绑定属性
+        // 只能覆盖使用 val 修饰的属性
+        val person: Person = new Teacher
+        println(person.name)
+
+
     }
 }
 
 
 class Person(){
-    var name : String = _
+    val name : String = "person"
     var age : Int = _
-
-    println("1. 父类的主构造器被调用")
-
-    def this(name:String, age:Int){
-        this()
-        println("2. 父类的辅助构造器被调用")
-        this.name = name
-        this.age = age
-    }
 
     def showInfo(): Unit = {
         println(s"人物的年龄: $name, 年龄: $age")
@@ -33,15 +41,21 @@ class Person(){
 
 class Student(name:String, age:Int) extends Person{
     var stdNo:String = _
-    println("3. 子类的主构造器被调用")
 
     def this(name:String, age:Int, stdNo:String){
         this(name, age)
-        println("4. 子类的辅助构造器被调用")
         this.stdNo = stdNo
     }
 
     override def showInfo(): Unit ={
         println(s"学生名字: $name, 年龄: $age, 学号: $stdNo")
     }
+}
+
+class Teacher extends Person{
+    override val name: String = "teacher"
+    override def showInfo(): Unit = {
+        println(s"Teacher")
+    }
+
 }
