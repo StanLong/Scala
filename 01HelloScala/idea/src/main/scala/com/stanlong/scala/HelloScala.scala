a7850dadd75292efb1b7e6924df6d6d876a09249
StanLong <youlinbhz@sina.cn>
2021-09-22 10:50:39 +0800

    伴生对象

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/HelloScala.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/HelloScala.scala
@@ -1,61 +1,45 @@
 package com.stanlong.scala
 
 /**
- * 多态
- * Java 中属性是静态绑定的，方法是动态绑定的
- * Scala 中属性和方法都是动态绑定的，需要加 override 关键字
+ * 单例对象（伴生对象）
  */
 object HelloScala {
     def main(args: Array[String]): Unit = {
-        val student = new Student("lisi", 19, "1142061140")
-        student.showInfo()
+        val student1 = Student.getInstance()
+        student1.printInfo()
 
-        val teacher = new Teacher
-        teacher.showInfo()
+        val student2 = Student.getInstance()
+        student2.printInfo()
 
-        // 动态绑定方法
-        def personInfo(person:Person): Unit ={
-            person.showInfo()
+        println(student1)
+        println(student2)
+
+    }
+}
+
+class Student private (val name:String, val age:Int){ // 参数列表前加 private 修饰符表示主构造器被私有化
+    def printInfo(): Unit ={
+        println(s"student:name=${name} , age=${age}, school=${Student.school}")
+    }
+}
+
+// 单例模式(饿汉式)
+// object Student{
+//     val school:String = "buaa"
+//     private val student: Student = new Student("zhangsan", 18)
+//     def getInstance():Student = student
+// }
+
+// 单例模式(饿汉式)
+object Student{
+    val school:String = "buaa"
+    private var student:Student = _
+    def getInstance():Student={
+        if(student == null){
+            // 如果没有对象实例，就创建一个
+            student = new Student("zhangsan", 18)
         }
-        personInfo(student)
-        personInfo(teacher)
-
-        // 动态绑定属性
-        // 只能覆盖使用 val 修饰的属性
-        val person: Person = new Teacher
-        println(person.name)
-
-
+        student
     }
 }
 
-
-class Person(){
-    val name : String = "person"
-    var age : Int = _
-
-    def showInfo(): Unit = {
-        println(s"人物的年龄: $name, 年龄: $age")
-    }
-}
-
-class Student(name:String, age:Int) extends Person{
-    var stdNo:String = _
-
-    def this(name:String, age:Int, stdNo:String){
-        this(name, age)
-        this.stdNo = stdNo
-    }
-
-    override def showInfo(): Unit ={
-        println(s"学生名字: $name, 年龄: $age, 学号: $stdNo")
-    }
-}
-
-class Teacher extends Person{
-    override val name: String = "teacher"
-    override def showInfo(): Unit = {
-        println(s"Teacher")
-    }
-
-}
