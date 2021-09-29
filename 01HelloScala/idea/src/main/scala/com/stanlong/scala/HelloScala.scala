71e7466712e9786c97e9badfb5c2c674a2975a5e
StanLong <youlinbhz@sina.cn>
2021-09-29 21:27:58 +0800

    集合

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/HelloScala.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/HelloScala.scala
@@ -1,44 +1,44 @@
 package com.stanlong.scala
 
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
 
 /**
- * 可变数组
+ * 可变列表
  */
 object HelloScala {
 
     def main(args: Array[String]): Unit = {
-        // 创建可变数组
-        val arr01 = ArrayBuffer[Int]()
-        val arr02 = ArrayBuffer(1,2,3,4)
-        println(arr02) // ArrayBuffer(1, 2, 3, 4) 底层调用的是 arr02.toString()
+        // 创建可变列表
+        val list0 = ListBuffer[Int](1, 2, 3)
+        val list1 = ListBuffer(12,23,45)
 
-        // 添加元素(在后面添加)
-        arr01.append(5) // 或者 arr01 += 5
-        println(arr01)
+        println(list0)
+        println(list1)
 
-        // 添加元素(在前面添加)
-        arr01.prepend(0) // 或者 0 +=: arr01
-        println(arr01)
+        // 添加元素
+        list0.append(5) // 在后面添加
+        list0.prepend(0) // 在前面添加
+        list0.insert(2,300) // 指定位置添加
 
-        // 指定索引位置添加
-        arr01.insert(1,2,3,4,5)
-        println(arr01)
+        // 合并列表
+        val list2 = list0 ++ list1
+        println(list0) // 不变
+        println(list1) // 不变
+        println(list2) // 合并
 
-        // 追加数组
-        arr01.insertAll(2, arr02) // 同样有 appendAll 和 prependAll
-        println(arr01)
+        list0 ++= list1 // 如果是 ++=: 那结果就是 list0 不变, list1 合并
+        println(list1) // 不变
+        println(list0) // 合并
 
+        // 修改元素
+        list1(0) = 0
+        list1.update(2, 20)
+        println(list1)
 
-        //删除
-        arr01.remove(0) // 删除索引 0 位置的元素
-        println(arr01)
-        arr01.remove(1, 4) // 从索引1开始，往后删除4个, 包含索引1的元素
-        println(arr01)
-        arr01 -= 5 // 直接删除元素值， 有就删除， 有重复的话只会删掉一个
-        println(arr01)
-
-        
+        // 删除
+        list1.remove(1)
+        list1 -= 45
+        println(list1)
     }
 }
 
