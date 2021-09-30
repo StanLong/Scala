57c7a503a1d885507257cc9025c0343e7b979ed6
StanLong <youlinbhz@sina.cn>
2021-09-30 23:02:04 +0800

    集合

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/HelloScala.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/HelloScala.scala
@@ -1,44 +1,34 @@
 package com.stanlong.scala
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
 
 /**
- * 可变列表
+ * 元组
+ * 可以存放各种相同或不同类型的数据
+ * 元组中最大只能有22个元素
  */
 object HelloScala {
 
     def main(args: Array[String]): Unit = {
-        // 创建可变列表
-        val list0 = ListBuffer[Int](1, 2, 3)
-        val list1 = ListBuffer(12,23,45)
 
-        println(list0)
-        println(list1)
+        // 创建
+        val tuple1 = (1, 2, 3, "hello", 4, true)
+        println(tuple1)
 
-        // 添加元素
-        list0.append(5) // 在后面添加
-        list0.prepend(0) // 在前面添加
-        list0.insert(2,300) // 指定位置添加
+        // 访问
+        println(tuple1._1) //访问元组的第 _x 个元素
+        println(tuple1._2)
+        println(tuple1._3)
+        println(tuple1.productElement(0)) // 按索引范围，从0开始
 
-        // 合并列表
-        val list2 = list0 ++ list1
-        println(list0) // 不变
-        println(list1) // 不变
-        println(list2) // 合并
+        // 遍历
+        for ( item <- tuple1.productIterator ) {
+            println(item)
+        }
 
-        list0 ++= list1 // 如果是 ++=: 那结果就是 list0 不变, list1 合并
-        println(list1) // 不变
-        println(list0) // 合并
-
-        // 修改元素
-        list1(0) = 0
-        list1.update(2, 20)
-        println(list1)
-
-        // 删除
-        list1.remove(1)
-        list1 -= 45
-        println(list1)
+        // 嵌套
+        val tuple2 = (1, 2, 3, "hello", 4, true, (1, 2, 3, "hello", 4, true))
+        println(tuple2._7._6)
     }
 }
 
