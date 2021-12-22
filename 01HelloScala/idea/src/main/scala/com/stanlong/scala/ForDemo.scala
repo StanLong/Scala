2ca5170378d2028ced704752496718113c91dece
StanLong <youlinbhz@sina.cn>
2021-12-22 21:56:01 +0800

    流程控制

--- 01HelloScala/idea/src/main/scala/com/stanlong/scala/ForDemo.scala
+++ 01HelloScala/idea/src/main/scala/com/stanlong/scala/ForDemo.scala
@@ -1,10 +1,45 @@
 package com.stanlong.scala
 
+import scala.util.control.Breaks.{break, breakable}
+
+/**
+ * 约瑟夫环
+ */
 object ForDemo {
     def main(args: Array[String]): Unit = {
-        var arr = new Array[Int](10)
-        for(item <- arr){
-            println("item=" + item)
+
+
+
+
+    }
+
+    // 模拟内存中的节点
+    class Node(in_data:Int){
+        var data = this.in_data
+        var next:Node = null
+
+        override def toString: String = "Node[" + "data=" + data + "]"
+    }
+
+    // 双向链表
+    class CircleSingleLinkedList{
+        var headNode = null // 定义头指针作为环形链表的引子
+        // 新增
+        def add(nums: Int): Unit ={
+            var temp = null // 定义赋值指针，帮助遍历
+            for(i <- 1 until(nums)){
+
+            }
+
+        }
+
+        def show(): Unit ={
+
+        }
+
+        def out(): Unit ={
+
         }
     }
+
 }
